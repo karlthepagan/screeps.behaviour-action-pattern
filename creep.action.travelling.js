4e5d9510e1ec36d2ff9b2da496c2ee17e5ff8002
Karl the Pagan <karlthepagan@gmail.com>
2017-02-08 23:54:26 -0800

    work in progress delivery task

--- creep.action.travelling.js
+++ creep.action.travelling.js
@@ -3,16 +3,40 @@ module.exports = action;
 action.isValidTarget = function(target){ return target != null; };
 action.isAddableAction = function(){ return true; };
 action.isAddableTarget = function(){ return true; };
-action.newTarget = function(creep){ return null; }
+action.newTarget = function(creep){
+    if( creep.data.travelPos || creep.data.travelRoom ) {
+        return creep;
+    }
+    return null;
+};
 action.step = function(creep){
     if(CHATTY) creep.say(this.name, SAY_PUBLIC);
     if( creep.target ){
         let pos;
-        if( creep.target.id == creep.id ) pos = new RoomPosition(25, 25, creep.data.travelRoom);
+        let targetRange = this.targetRange;
+        if( creep.target.id == creep.id) {
+            if( creep.data.travelPos ) {
+                pos = new RoomPosition(creep.data.travelPos.x, creep.data.travelPos.y, creep.data.travelPos.roomName);
+            } else if( !creep.data.travelRoom ) {
+                logError('no travel room', {creepName:creep.name, roomName:creep.room.name, Behaviour:creep.data.creepType, Action:'travelling'});
+            } else if( creep.room.getBorder(creep.data.travelRoom) ) {
+                logError('bad border target', {creepName:creep.name, roomName:creep.room.name, Behaviour:creep.data.creepType, Action:'travelling'});
+                targetRange = 24;
+                pos = new RoomPosition(25, 25, creep.data.travelRoom);
+            } else {
+                targetRange = 24;
+                pos = new RoomPosition(25, 25, creep.data.travelRoom);
+            }
+            if( creep.pos.roomName === pos.roomName ) {
+                delete creep.target;
+                delete creep.data.travelRoom;
+                delete creep.data.travelPos;
+            }
+        }
         else pos = creep.target.pos;
         creep.drive( pos, this.reachedRange, this.targetRange, Infinity );
     }
-    if( !creep.target || creep.target.pos.roomName == creep.pos.roomName ){
+    if( !creep.target || creep.target !== creep && creep.target.pos.roomName == creep.pos.roomName ){
         // unregister
         delete creep.action;
         delete creep.target;
